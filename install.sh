e6580e9ba4755da37a8fdecae1b77b830cf83a6a
zhuowenzhao <zwenzhao11@gmail.com>
2023-03-28 00:30:52 -0700

    updated install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,9 @@ docker-compose -f docker-compose/docker-compute-api.yml up -d
 sleep 10
 
 . docker-compose/.env
+
 docker cp data/dump mongodb:/dump
-docker exec -i mongodb /usr/bin/mongorestore --username $MONGO_DB_USERNAME --password $MONGO_DB_PASSWORD --authenticationDatabase admin --db content_registry /dump/lbl-mlexchange
+docker exec -i mongodb /usr/bin/mongorestore --username=$MONGO_DB_USERNAME --password=$MONGO_DB_PASSWORD --authenticationDatabase=admin --db=content_registry --drop /dump/lbl-mlexchange
 
 docker-compose -f docker-compose/docker-content-registry.yml up -d
 docker-compose -f docker-compose/docker-model-images.yml pull
