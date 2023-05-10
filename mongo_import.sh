59b23dc50062519a936716d8cc3ffb9347ae350b
taxe10 <tanchavez@lbl.gov>
2023-05-09 19:57:59 -0700

    adding master docker-compose

---
+++ mongo_import.sh
@@ -0,0 +1,1 @@
+/usr/bin/mongorestore --username=${MONGO_INITDB_ROOT_USERNAME} --password=${MONGO_INITDB_ROOT_PASSWORD} --authenticationDatabase=admin --db=content_registry --drop /dump/lbl-mlexchange
